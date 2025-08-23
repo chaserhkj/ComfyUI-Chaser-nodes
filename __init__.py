ee5f090cbed6797818dc18afcc4e90a917f1f25a
Chaser Huang <huangkangjing@gmail.com>
2025-08-22 20:36:19 -0400

    node registration

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .webdav import UploadImagesToWebDAV
+
+NODE_CLASS_MAPPINGS = {
+    "UploadImagesToWebDAV": UploadImagesToWebDAV,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "UploadImagesToWebDAV": "Upload image(s) to WebDAV",
+}
